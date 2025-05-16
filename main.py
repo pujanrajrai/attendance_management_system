6dfb2aa24e245401532b7b49ae9d1dec5369c444
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2025-05-16 07:52:22 +0545

    while loop implemented

--- main.py
+++ main.py
@@ -11,22 +11,26 @@ def update_student_record():
 
 
 def menu():
-    print("Etner 1 To Add Student")
-    print("Enter 2 To View Student Details")
-    print("Etner 3 to Update Student")
-    print("Etner 4 To Delete Student")
-    print("Etner 5 To Take Attendance")
-    print("Enter 6 to View Attendance")
-    print("Enter 8 to end")
-    option = input()
-    if option == "1":
-        add_student_record()
-    elif option == "2":
-        view_student_record()
-    elif option == "3":
-        update_student_record()
-    else:
-        print("Invalid choices")
+    run = True
+    while run:
+        print("Etner 1 To Add Student")
+        print("Enter 2 To View Student Details")
+        print("Etner 3 to Update Student")
+        print("Etner 4 To Delete Student")
+        print("Etner 5 To Take Attendance")
+        print("Enter 6 to View Attendance")
+        print("Enter 8 to end")
+        option = input()
+        if option == "1":
+            add_student_record()
+        elif option == "2":
+            view_student_record()
+        elif option == "3":
+            update_student_record()
+        elif option == "8":
+            run = False
+        else:
+            print("Invalid choices")
 
 
 menu()
